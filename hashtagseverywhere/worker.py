00661857ee7cb01001eee88164515d67767ffae3
Saurabh Mathur <saurabhmathur96@gmail.com>
2016-05-15 18:56:03 +0530

    fix worker.py

--- hashtagseverywhere/worker.py
+++ hashtagseverywhere/worker.py
@@ -7,13 +7,13 @@ listen = ['default']
 
 
 
-host = os.genenv('REDIS_HOST')
+host = os.getenv('REDIS_HOST')
 if host is None:
     redis_url = 'redis://localhost:6379'
     conn = redis.from_url(redis_url)
 else :
-    port = os.genenv('REDIS_PORT')
-    password = os.genenv('REDIS_PASSWORD')
+    port = os.getenv('REDIS_PORT')
+    password = os.getenv('REDIS_PASSWORD')
     conn = redis.Redis(host=host,port=port,password=password)
 
 def main():
